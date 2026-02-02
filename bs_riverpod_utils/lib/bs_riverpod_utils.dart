630db9b6ed9db9d7b21a2fd975e54f62ec1c65d8
MyMac <bashier_al-saed@outlook.com>
2026-02-02 08:03:31 +0200

    doc(riverpod_utils)

--- bs_riverpod_utils/lib/bs_riverpod_utils.dart
+++ bs_riverpod_utils/lib/bs_riverpod_utils.dart
@@ -2,13 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/misc.dart';
 
+/// Convenience extension that allows non-watching operations
+/// on providers using context
 extension Context on BuildContext {
-  // Custom call a provider for reading method only
-  // It will be helpful for us for calling the read function
-  // without Consumer,ConsumerWidget or ConsumerStatefulWidget
-  // In case if you face any issue using this then please wrap your widget
-  // with consumer and then call your provider
-
   T read<T>(ProviderListenable<T> p) => ProviderScope.containerOf(this, listen: false).read(p);
 
   void invalidate<T>(ProviderBase<T> p) =>
